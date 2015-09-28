806ded49ecac0a643af132c318da057750e5e5d7
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-09-28 12:58:35 -0500

    s/mvn/gradle/

--- src/main/java/us/yellosoft/storm/tutorial/WordCountTopology.java
+++ src/main/java/us/yellosoft/storm/tutorial/WordCountTopology.java
@@ -2,10 +2,7 @@ package us.yellosoft.storm.tutorial;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
 import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.IBolt;
-import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.topology.base.BaseBasicBolt;
@@ -28,18 +25,27 @@ import java.util.HashMap;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class WordCountTopology {
+/** Word count frequency topology */
+public final class WordCountTopology {
+  /** Utility class */
+  private WordCountTopology() {}
+
+  /** Text generator */
   public static class TextSpout extends BaseRichSpout implements IRichSpout {
-    public static final String TEXT_DIRECTORY="resources/sherlock-holmes/";
+    public static final long serialVersionUID = 1L;
+
+    public static final String TEXT_DIRECTORY = "resources/sherlock-holmes/";
 
     private SpoutOutputCollector collector;
 
     private Queue<String> lines = new PriorityQueue<String>();
 
-    public TextSpout() throws FileNotFoundException, IOException {
+    /** Construct a TextSpout
+        @throws IOException on IO error
+     */
+    public TextSpout() throws IOException {
       Collection<File> textFiles = FileUtils.listFiles(new File(TEXT_DIRECTORY), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
 
       for (File textFile:textFiles) {
@@ -56,23 +62,46 @@ public class WordCountTopology {
       }
     }
 
-    public void open(Map stormConf, TopologyContext context, SpoutOutputCollector collector) {
+    /** Open the spout
+        @param stormConf Storm configuration
+        @param context topology
+        @param collector text emitter
+     */
+    public void open(
+      final Map stormConf,
+      final TopologyContext context,
+      final SpoutOutputCollector collector
+    ) {
       this.collector = collector;
     }
 
+    /** Generate next text */
     public void nextTuple() {
       if (lines.peek() != null) {
         collector.emit(new Values(lines.poll()));
       }
     }
 
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    /** Specify emitted fields
+        @param declarer Storm specification
+     */
+    public void declareOutputFields(final OutputFieldsDeclarer declarer) {
       declarer.declare(new Fields("line"));
     }
   }
 
+  /** Word splitting bolt */
   public static class WordSplitter extends BaseBasicBolt {
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
+    public static final long serialVersionUID = 1L;
+
+    /** Split text into words
+        @param tuple text
+        @param collector word emitter
+     */
+    public void execute(
+      final Tuple tuple,
+      final BasicOutputCollector collector
+    ) {
       String[] words = tuple.getString(0).split(" ");
 
       for (String word:words) {
@@ -82,45 +111,65 @@ public class WordCountTopology {
       }
     }
 
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    /** Emitter specification
+        @param declarer Storm specification
+     */
+    public void declareOutputFields(final OutputFieldsDeclarer declarer) {
       declarer.declare(new Fields("word"));
     }
   }
 
+  /** Frequency counter */
   public static class WordCount extends BaseBasicBolt {
+    public static final long serialVersionUID = 1L;
+
     private Map<String, Integer> frequencies = new HashMap<String, Integer>();
 
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
-      String word = tuple.getString(0);
+    /** Count word frequencies
+        @param tuple words
+        @param collector frequency emitter
+     */
+    public void execute(
+      final Tuple tuple,
+      final BasicOutputCollector collector
+    ) {
+      final String word = tuple.getString(0);
       Integer oldFrequency = frequencies.get(word);
 
       if (oldFrequency == null) {
         oldFrequency = 0;
       }
 
-      Integer newFrequency = oldFrequency + 1;
+      final Integer newFrequency = oldFrequency + 1;
 
       frequencies.put(word, newFrequency);
 
       collector.emit(new Values(word, newFrequency));
     }
 
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    /** Emitter specification
+        @param declarer Storm specification
+     */
+    public void declareOutputFields(final OutputFieldsDeclarer declarer) {
       declarer.declare(new Fields("word", "frequency"));
     }
   }
 
-  public static void main(String[] args) throws Exception {
-    TopologyBuilder builder = new TopologyBuilder();
+  /** CLI entry point
+      @param args CLI flags
+      @throws Exception on error
+   */
+  public static void main(final String[] args) throws Exception {
+    final TopologyBuilder builder = new TopologyBuilder();
     builder.setSpout("text-spout", new TextSpout(), 1);
     builder.setBolt("word-splitter", new WordSplitter(), 4).shuffleGrouping("text-spout");
     builder.setBolt("word-counter", new WordCount(), 8).fieldsGrouping("word-splitter", new Fields("word"));
 
-    Config config = new Config();
+    final Config config = new Config();
     config.setDebug(true);
     config.setMaxTaskParallelism(3);
 
-    LocalCluster cluster = new LocalCluster();
+    final LocalCluster cluster = new LocalCluster();
     cluster.submitTopology("word-count", config, builder.createTopology());
   }
 }
